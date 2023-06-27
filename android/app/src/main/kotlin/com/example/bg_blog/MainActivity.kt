ee54bed0da1071972373be9dd12ba28b09862ba4
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2023-06-28 02:01:47 +0300

    feat: Initial commit

---
+++ android/app/src/main/kotlin/com/example/bg_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bg_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/article.dart
@@ -0,0 +1,14 @@
+class ArticlePost {
+  final String title;
+  final String text;
+  final String author;
+  final String date;
+  final String picture;
+
+  ArticlePost(
+      {required this.title,
+      required this.text,
+      required this.author,
+      required this.date,
+      required this.picture});
+}
